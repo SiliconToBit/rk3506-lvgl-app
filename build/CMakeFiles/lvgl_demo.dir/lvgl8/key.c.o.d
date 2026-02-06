bcb6474d1965f3eef09d2d451a79c546c1f69e7b
SiliconToBit <gm.me@qq.com>
2026-02-06 22:02:34 +0800

    Chore: Add .gitignore and remove ignored files from index

--- build/CMakeFiles/lvgl_demo.dir/lvgl8/key.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/lvgl_demo.dir/lvgl8/key.c.o: \
- /home/gm/Workspace/linux_sdk/luckfox_rk3506_sdk/app/lvgl_app/lvgl8/key.c \
- /home/gm/Workspace/linux_sdk/luckfox_rk3506_sdk/buildroot/output/rockchip_rk3506_luckfox/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/stdc-predef.h \
- /home/gm/Workspace/linux_sdk/luckfox_rk3506_sdk/app/lvgl_app/lvgl8/key.h
--- build/CMakeFiles/lvgl_demo.dir/src/fonts/fonts.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/lvgl_demo.dir/src/fonts/fonts.c.o: \
- /home/gm/Workspace/linux_sdk/luckfox_rk3506_sdk/app/lvgl_app/src/fonts/fonts.c \
- /home/gm/Workspace/linux_sdk/luckfox_rk3506_sdk/buildroot/output/rockchip_rk3506_luckfox/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/stdc-predef.h \
- /home/gm/Workspace/linux_sdk/luckfox_rk3506_sdk/app/lvgl_app/src/fonts/qweather_icons.h
